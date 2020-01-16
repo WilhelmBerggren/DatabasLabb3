876b48f7c2ef7423f6c177da67cf68e5c05ec798
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-01-16 14:30:05 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabasLabb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
