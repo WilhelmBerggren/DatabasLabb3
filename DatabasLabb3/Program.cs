e8963b34302997749d7dedda637c7919099e557d
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-01-17 13:46:53 +0100

    Added MongoDB.Driver

--- DatabasLabb3/Program.cs
+++ DatabasLabb3/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 
+using MongoDB.Driver;
+
 namespace DatabasLabb3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
+            var db = mongoClient.GetDatabase("test");
             Console.WriteLine("Hello World!");
         }
     }
