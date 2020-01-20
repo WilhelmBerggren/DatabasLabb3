ee9f602c118571afe82bc14d7e2283dd3bdb3e5d
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-01-20 13:48:26 +0100

    Finished VG (team effort ^^)

--- DatabasLabb3/Program.cs
+++ DatabasLabb3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using MongoDB.Bson;
+using MongoDB.Bson.IO;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 
@@ -10,24 +11,35 @@ namespace DatabasLabb3
     {
         static void Main(string[] args)
         {
-            MongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
-            var db = mongoClient.GetDatabase("labb3");
+            //Create connection with local server
+            MongoClient mongoClient = new MongoClient("mongodb://localhost:27017"); 
+
+            //Create reference to database "labb3"
+            var db = mongoClient.GetDatabase("labb3"); 
+
+            //If collection "restaurants" exists, drop it
+            db.DropCollection("restaurants");
+
+            //Create reference to collection "restaurants"
             var restaurants = db.GetCollection<Restaurant>("restaurants");
 
             InsertRestaurants(restaurants);
-            PrintRestaurants(restaurants);
-        }
 
-        private static void PrintRestaurants(IMongoCollection<Restaurant> restaurants)
-        {
-            foreach(Restaurant restaurant in restaurants.Find(new BsonDocument()).ToEnumerable())
-            {
-                Console.WriteLine($"{restaurant.Name}");
-            }
+            PrintRestaurants(restaurants);
+            PrintCafes(restaurants);
+            
+            IncrementXYZRating(restaurants);
+            PrintRestaurants(restaurants);
+            
+            UpdateCookieName(restaurants);
+            PrintRestaurants(restaurants);
+
+            FindFourStarsAndAbove(restaurants);
         }
 
         private static void InsertRestaurants(IMongoCollection<Restaurant> restaurants)
         {
+            //Create array of class Restaurant
             var restaurantList = new Restaurant[] {
                 new Restaurant {
                     Name = "Sun Bakery Trattoria",
@@ -56,10 +68,111 @@ namespace DatabasLabb3
                 }
             };
 
+            //Insert all Restaurants in collection
             restaurants.InsertMany(restaurantList);
         }
+
+        private static void PrintRestaurants(IMongoCollection<Restaurant> restaurants)
+        {
+            Console.WriteLine("\nAll restaurants:\n");
+            //Find all Restaurants in database collection, and print their name
+            foreach (Restaurant restaurant in restaurants.Find(new BsonDocument()).ToEnumerable())
+            {
+                Console.WriteLine($"{restaurant.ToJson()}");
+            }
+        }
+        private static void PrintCafes(IMongoCollection<Restaurant> restaurants)
+        {
+            Console.WriteLine("\nCafés:\n");
+            //Create filter to find documents with "Cafe" in "categories"
+            var filter = Builders<Restaurant>.Filter.Eq("categories", "Cafe");
+
+            //Create projection to exclude "_id" and include "name"
+            var projection = Builders<Restaurant>.Projection.Exclude("_id").Include("name");
+
+            //Enumerate over all restaurants matching filter and project them using projection, print their name
+            foreach (var restaurant in restaurants.Find(filter).Project(projection).ToEnumerable())
+            {
+                Console.WriteLine($"{restaurant.ToJson()}");
+            }
+        }
+
+        private static void IncrementXYZRating(IMongoCollection<Restaurant> restaurants)
+        {
+            Console.WriteLine("\nIncrementing XYZ Coffee Bar stars by 1\n");
+
+            //Create filter to find documents with "name" of "XYZ Coffee Bar"
+            var filter = Builders<Restaurant>.Filter.Eq("name", "XYZ Coffee Bar");
+
+            //Create action to update field "stars" by incrementing with value 1
+            var action = Builders<Restaurant>.Update.Inc("stars", 1);
+
+            //Perform action on one item matching filter
+            restaurants.UpdateOne(filter, action);
+        }
+
+        private static void UpdateCookieName(IMongoCollection<Restaurant> restaurants)
+        {
+            Console.WriteLine("\nChanging name of \"456 Cookies Shop\" to \"123 Cookies Heaven:\"\n");
+
+            //Create filter to find documents with "name" of "456 Cookies Shop"
+            var filter = Builders<Restaurant>.Filter.Eq("name", "456 Cookies Shop");
+
+            //Create action to set field "name" to "123 Cookies Heaven"
+            var action = Builders<Restaurant>.Update.Set("name", "123 Cookies Heaven");
+
+            //Perform action on one item matching filter
+            restaurants.UpdateOne(filter, action);
+        }
+
+        private static void FindFourStarsAndAbove(IMongoCollection<Restaurant> restaurants)
+        {
+            // create matching aggregate stage to only include restaurants with a star rating of 4 or more
+
+            Console.WriteLine("\nRestaurants with a star rating of 4 or more:\n");
+
+            var match = new BsonDocument
+            {
+                { "$match",
+                    new BsonDocument
+                    {
+                        { "stars",
+                            new BsonDocument
+                            {
+                                { "$gte", 4 }
+                            }
+                        }
+                    }
+                }
+            };
+
+            // create projection aggregate stage to exclude field "_id" and include fields "name" and "stars"
+            var project = new BsonDocument
+            {
+                { "$project", 
+                    new BsonDocument
+                    {
+                        { "_id", 0 },
+                        { "name", 1 },
+                        { "stars", 1 }
+                    } 
+                }
+            };
+
+            // send array of aggregate pipeline stages to Aggregate, convert from cursor to enumerable to indented json
+            var response = restaurants
+                .Aggregate<BsonDocument>(new[] { match, project })
+                .ToEnumerable()
+                .ToJson(new JsonWriterSettings { Indent = true }
+            );
+            Console.WriteLine(response);
+        }
     }
-  
+
+    /*
+     * Restaurant class to represent a document in collection Restaurants
+     * [BsonId] matches with _Id, [BsonElement("thing")] with field "thing" 
+     */
     class Restaurant
     {
         [BsonId]
