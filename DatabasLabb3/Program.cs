5f210621809bcc2681ab88e93ac9826932b3e654
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2020-01-20 14:04:46 +0100

    Removed Restaurant class and switched all references to BsonDocument

--- DatabasLabb3/Program.cs
+++ DatabasLabb3/Program.cs
@@ -21,7 +21,7 @@ namespace DatabasLabb3
             db.DropCollection("restaurants");
 
             //Create reference to collection "restaurants"
-            var restaurants = db.GetCollection<Restaurant>("restaurants");
+            var restaurants = db.GetCollection<BsonDocument>("restaurants");
 
             InsertRestaurants(restaurants);
 
@@ -37,34 +37,34 @@ namespace DatabasLabb3
             FindFourStarsAndAbove(restaurants);
         }
 
-        private static void InsertRestaurants(IMongoCollection<Restaurant> restaurants)
+        private static void InsertRestaurants(IMongoCollection<BsonDocument> restaurants)
         {
             //Create array of class Restaurant
-            var restaurantList = new Restaurant[] {
-                new Restaurant {
-                    Name = "Sun Bakery Trattoria",
-                    Stars = 4,
-                    Categories = new List<string>() {"Pizza", "Pasta", "Italian", "Coffee", "Sandwiches"}
+            var restaurantList = new BsonDocument[] {
+                new BsonDocument {
+                    { "name", "Sun Bakery Trattoria" },
+                    { "stars", 4 },
+                    { "categories", new BsonArray {"Pizza", "Pasta", "Italian", "Coffee", "Sandwiches"} }
                 },
-                new Restaurant {
-                    Name = "Blue Bagels Grill",
-                    Stars = 3,
-                    Categories = new List<string>() {"Bagels", "Cookies", "Sandwiches"}
+                new BsonDocument {
+                    { "name", "Blue Bagels Grill" },
+                    { "stars", 3 },
+                    { "categories", new BsonArray {"Bagels", "Cookies", "Sandwiches"} }
                 },
-                new Restaurant {
-                    Name = "Hot Bakery Cafe",
-                    Stars = 4,
-                    Categories = new List<string>() {"Bakery", "Cafe", "Coffee", "Dessert"}
+                new BsonDocument {
+                    { "name", "Hot Bakery Cafe" },
+                    { "stars", 4 },
+                    { "categories", new BsonArray {"Bakery", "Cafe", "Coffee", "Dessert"} }
                 },
-                new Restaurant {
-                    Name = "XYZ Coffee Bar",
-                    Stars = 5,
-                    Categories = new List<string>() {"Coffee", "Cafe", "Bakery", "Chocolates"}
+                new BsonDocument {
+                    { "name", "XYZ Coffee Bar" },
+                    { "stars", 5 },
+                    { "categories", new BsonArray {"Coffee", "Cafe", "Bakery", "Chocolates"} }
                 },
-                new Restaurant {
-                    Name = "456 Cookies Shop",
-                    Stars = 4,
-                    Categories = new List<string>() {"Bakery", "Cookies", "Cake", "Coffee"}
+                new BsonDocument {
+                    { "name", "456 Cookies Shop" },
+                    { "stars", 4 },
+                    { "categories", new BsonArray {"Bakery", "Cookies", "Cake", "Coffee"} }
                 }
             };
 
@@ -72,23 +72,23 @@ namespace DatabasLabb3
             restaurants.InsertMany(restaurantList);
         }
 
-        private static void PrintRestaurants(IMongoCollection<Restaurant> restaurants)
+        private static void PrintRestaurants(IMongoCollection<BsonDocument> restaurants)
         {
             Console.WriteLine("\nAll restaurants:\n");
             //Find all Restaurants in database collection, and print their name
-            foreach (Restaurant restaurant in restaurants.Find(new BsonDocument()).ToEnumerable())
+            foreach (var restaurant in restaurants.Find(new BsonDocument()).ToEnumerable())
             {
                 Console.WriteLine($"{restaurant.ToJson()}");
             }
         }
-        private static void PrintCafes(IMongoCollection<Restaurant> restaurants)
+        private static void PrintCafes(IMongoCollection<BsonDocument> restaurants)
         {
             Console.WriteLine("\nCafés:\n");
             //Create filter to find documents with "Cafe" in "categories"
-            var filter = Builders<Restaurant>.Filter.Eq("categories", "Cafe");
+            var filter = Builders<BsonDocument>.Filter.Eq("categories", "Cafe");
 
             //Create projection to exclude "_id" and include "name"
-            var projection = Builders<Restaurant>.Projection.Exclude("_id").Include("name");
+            var projection = Builders<BsonDocument>.Projection.Exclude("_id").Include("name");
 
             //Enumerate over all restaurants matching filter and project them using projection, print their name
             foreach (var restaurant in restaurants.Find(filter).Project(projection).ToEnumerable())
@@ -97,35 +97,35 @@ namespace DatabasLabb3
             }
         }
 
-        private static void IncrementXYZRating(IMongoCollection<Restaurant> restaurants)
+        private static void IncrementXYZRating(IMongoCollection<BsonDocument> restaurants)
         {
             Console.WriteLine("\nIncrementing XYZ Coffee Bar stars by 1\n");
 
             //Create filter to find documents with "name" of "XYZ Coffee Bar"
-            var filter = Builders<Restaurant>.Filter.Eq("name", "XYZ Coffee Bar");
+            var filter = Builders<BsonDocument>.Filter.Eq("name", "XYZ Coffee Bar");
 
             //Create action to update field "stars" by incrementing with value 1
-            var action = Builders<Restaurant>.Update.Inc("stars", 1);
+            var action = Builders<BsonDocument>.Update.Inc("stars", 1);
 
             //Perform action on one item matching filter
             restaurants.UpdateOne(filter, action);
         }
 
-        private static void UpdateCookieName(IMongoCollection<Restaurant> restaurants)
+        private static void UpdateCookieName(IMongoCollection<BsonDocument> restaurants)
         {
             Console.WriteLine("\nChanging name of \"456 Cookies Shop\" to \"123 Cookies Heaven:\"\n");
 
             //Create filter to find documents with "name" of "456 Cookies Shop"
-            var filter = Builders<Restaurant>.Filter.Eq("name", "456 Cookies Shop");
+            var filter = Builders<BsonDocument>.Filter.Eq("name", "456 Cookies Shop");
 
             //Create action to set field "name" to "123 Cookies Heaven"
-            var action = Builders<Restaurant>.Update.Set("name", "123 Cookies Heaven");
+            var action = Builders<BsonDocument>.Update.Set("name", "123 Cookies Heaven");
 
             //Perform action on one item matching filter
             restaurants.UpdateOne(filter, action);
         }
 
-        private static void FindFourStarsAndAbove(IMongoCollection<Restaurant> restaurants)
+        private static void FindFourStarsAndAbove(IMongoCollection<BsonDocument> restaurants)
         {
             // create matching aggregate stage to only include restaurants with a star rating of 4 or more
 
@@ -167,21 +167,5 @@ namespace DatabasLabb3
             );
             Console.WriteLine(response);
         }
-    }
-
-    /*
-     * Restaurant class to represent a document in collection Restaurants
-     * [BsonId] matches with _Id, [BsonElement("thing")] with field "thing" 
-     */
-    class Restaurant
-    {
-        [BsonId]
-        public ObjectId ID { get; set; }
-        [BsonElement("name")]
-        public string Name { get; set; }
-        [BsonElement("stars")]
-        public int Stars { get; set; }
-        [BsonElement("categories")]
-        public List<string> Categories { get; set; }
     }
 }
